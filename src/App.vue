d0ea1da74676acb274952e0282b56d6203f14e4a
Loe Lindström <lht.lindstrom@gmail.com>
2023-01-03 09:36:49 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<!-- 
+Inspiration:
+https://stackoverflow.com/questions/73147462/binding-a-media-feed-stream-to-video-element-vue-3-composition-api 
+-->
+
+
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+import PushButton from "./components/PushButton.vue";
+
+const stream = ref(null)
+const constraints = {
+  audio: false,
+  video: true,
+  // video: {
+    // width: { min: 1024, ideal: 1280, max: 1920 },
+    // height: { min: 576, ideal: 720, max: 1080 },
+  // },
+}
+
+onMounted(async () => {
+  stream.value = await navigator.mediaDevices.getUserMedia(constraints)
+})
+
+onBeforeUnmount(() => {
+  stream.value.getTracks().forEach(track => track.stop())
+})
+</script>
+
+<template>
+  <div class="camera">
+    <div class="camera-lens">
+      <video :srcObject="stream" autoplay></video>
+    </div>
+    <div class="push-button-container">
+      <PushButton class="push-button" />
+    </div>
+  </div>
+</template>
+
+<style>
+* {
+  height: 100%;
+  width: 100%;
+  padding: 0%;
+}
+
+#app {
+  background: url('assets/camera-background.svg');
+  background-size: cover;
+}
+
+.push-button-container {
+  width: 15%;
+  height: 15%;
+  display: block;
+  padding: 15px;
+  /* margin: auto; */
+  margin-left: 70%;
+}
+
+.camera-lens {
+  border: 5px solid rgb(34, 34, 34);
+  background-color: black;
+  width: 30%;
+  height: 30%;
+  margin: auto;
+  padding: 1%;
+}
+</style>
---
+++ src/components/PushButton.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+    <button class="button-side">
+        <span class="button-top"></span>
+    </button>
+</template>
+
+<style scoped>  
+.button-side {
+    background: hsl(40deg 100% 32%);
+    border-radius: 50%;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+    outline-offset: 4px;
+}
+.button-top {
+    display: block;
+    padding: 10% 15%;
+    border-radius: 50%;
+    font-size: 1.25rem;
+    background: hsl(45deg 100% 47%);
+    color: white;
+    transform: translateY(-6px);
+}
+
+.button-side:active .button-top {
+    transform: translateY(-2px);
+}
+</style>
